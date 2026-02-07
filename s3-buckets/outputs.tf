812de730c50a3c02a39f507d719ecfe227466c2e
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2026-02-07 23:11:21 +0530

    Update outputs.tf

--- s3-buckets/outputs.tf
+++ s3-buckets/outputs.tf
@@ -1,9 +1,9 @@
-output "bucket_id" {
+output "bucket_id1" {
   description = "ID of the first S3 bucket"
   value = aws_s3_bucket.bucket1.id
 }
 
-output "bucket_id" {
+output "bucket_id2" {
   description = "ID of the second S3 bucket"
   value = aws_s3_bucket.bucket2.id
 }
